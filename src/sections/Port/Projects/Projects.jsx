f2f45c8109a1b4f276fe873def7f43a23127c6f7
Dhinesh <dhineshkbd3@email>
2024-11-20 18:32:12 +0530

    latest

--- src/sections/Port/Projects/Projects.jsx
+++ src/sections/Port/Projects/Projects.jsx
@@ -5,6 +5,10 @@ import freshBurger from '../../../assets/calculator.png'
 import hipster from '../../../assets/Currency.png'
 import ProjectCard from '../../../common/ProjectCard'
 import fitlift from '../../../assets/deccan airways (2).png'
+import icon from '../../../assets/icon.png'
+import clock from '../../../assets/clock.png'
+import stopwatch from '../../../assets/Stopwatch.png'
+import car from '../../../assets/car.png'
 
 function Projects() {
   return ( 
@@ -23,6 +27,18 @@ function Projects() {
           <ProjectCard src={fitlift} link='https://github.com/DHINESH-png/Deccan-Air.git'
          h3='Deccan Air'
          p='Airways'/>
+         <ProjectCard src={icon} link='https://github.com/DHINESH-png/To-Do'
+         h3='Bucket-List'
+         p='To-Do App'/>
+          <ProjectCard src={clock} link='https://github.com/DHINESH-png/Clock'
+         h3='MyClock'
+         p='Digital Clock'/>
+          <ProjectCard src={stopwatch} link='https://github.com/DHINESH-png/Stopwatch'
+         h3='Get, Set & Go'
+         p='StopWatch'/>
+          <ProjectCard src={car} link='https://github.com/DHINESH-png/EV-olution'
+         h3='EV-olution'
+         p='EV-Car'/>
         </div>
     </section>
   )
